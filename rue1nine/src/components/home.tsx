3502ef3662085d5f8d439e3f52f75291763ee7c9
Pugsy <nimsarakarunaratne@gmail.com>
2026-02-19 23:04:26 +0530

    adding men women sections to home

--- rue1nine/src/components/home.tsx
+++ rue1nine/src/components/home.tsx
@@ -16,6 +16,28 @@ function Home() {
         <div className="min-h-screen bg-cover bg-center flex items-center justify-center w-full overflow-hidden" 
             style={{backgroundImage: `url(${assets.hero})`}} 
             id='Header'>
+
+        
+            <div className="flex flex-row">
+                <motion.a href="#MenSection" 
+                initial = {{y:'100vw', opacity:0}}
+                animate={{y:0, opacity:1}}
+                transition={{duration:1.5, ease:"easeOut"}}
+                className="mt-30 text-red-500 text-2xl font-light hover:underline transition-all duration-2000 ease-in-out">
+                    <img src={assets.tshirt1a}/>
+                    <p>Men's Clothing</p>
+                </motion.a>
+        
+                <motion.a href="#WomenSection" 
+                initial = {{y:'100vw', opacity:0}}
+                animate={{y:0, opacity:1}}
+                transition={{duration:1.5, ease:"easeOut"}}
+                className="mt-30 text-red-500 text-2xl font-light hover:underline transition-all duration-2000 ease-in-out">
+                   <img src={assets.tshirt1b}/>
+                  <p>Women's Clothing</p>
+                </motion.a>
+        
+                  </div>
             
             <motion.a href="#RuesWorld" 
             initial = {{y:'100vw', opacity:0}}
---
+++ rue1nine/src/components/mensection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function mensection() {
+  return (
+    <div id='MenSection'>mensection</div>
+  )
+}
+
+export default mensection
