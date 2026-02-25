cd548efc9d5bdb167f58377cda9c83da6f11dea0
HarishRock0 <hariksank.23@gmail.com>
2026-02-25 16:54:23 +0530

    fix: correct className in Navbar and NewArrivals components for consistency

--- rue1nine/src/components/navbar.tsx
+++ rue1nine/src/components/navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
     animate={{ y:'0%',  opacity:1}}
     transition={{ duration: 0.3, ease: "easeOut" }}
     >
-        <div className="container mx-auto  flex justify-between items-center px-6 md:px-20 lg:px-32 bg-white/30 backdrop-blur-md shadow-md py-8">
+        <div className="w-full flex justify-between items-center px-6 md:px-20 lg:px-32 bg-white/30 backdrop-blur-md shadow-md py-8">
             <ul className="hidden md:flex gap-7">
                 <img src={assets.logo_light} alt="Logo" className="w-32" 
                 onClick={()=> {
